a9b3436b942c1f0c1678053c6d7b04a38ceee85a
heberGustavo <heber.gbarbosa@hotmail.com>
2024-07-30 12:46:45 -0300

    Create Banner Component

--- app-movies/src/App.js
+++ app-movies/src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <>
       <Header />
+      <Banner />
       <h2>Hello World!</h2>
       <p>I'm lerning React JS</p>
       <Footer />
