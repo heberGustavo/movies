2c0f99a48e7c96f2789a97e252e839aab0fc1ebb
heberGustavo <heber.gbarbosa@hotmail.com>
2024-07-30 09:07:43 -0300

    Create Footer Component

---
+++ app-movies/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import style from './Footer.module.css'
+
+function Footer(){
+    return(
+        <>
+            <footer className={style.footer}>
+                <span>HG Movies &copy; Desenvolvido por Heber Gustavo 2024</span>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
