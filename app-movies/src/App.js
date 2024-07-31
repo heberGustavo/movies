dc61ff9384a55a8922b37d770f88960d1bf18ef2
heberGustavo <heber.gbarbosa@hotmail.com>
2024-07-31 07:39:15 -0300

    Export of the Function and Const of Category.js to App.js

--- app-movies/src/App.js
+++ app-movies/src/App.js
@@ -3,21 +3,7 @@ import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from "./components/Card";
-import Category from "./components/Category";
-
-import videos from "./json/videos.json";
-
-const categories = [
-  "Geografia",
-  "Como fazer e usar",
-  "Astronomia e Geografia",
-  "Climatologia, Meteorologia, Vegetação",
-  "Geologia e Hidrografia",
-];
-
-function filterCategory(id){
-  return videos.filter(video => video.category === categories[id]);
-}
+import Category, {categories, filterCategory} from "./components/Category";
 
 function App() {
   return (
