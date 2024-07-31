67269521c426691fafedf3159d241cf9167d1411
heberGustavo <heber.gbarbosa@hotmail.com>
2024-07-31 07:27:12 -0300

    Create Category Component and getting data by category

--- app-movies/src/App.js
+++ app-movies/src/App.js
@@ -3,8 +3,21 @@ import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from "./components/Card";
+import Category from "./components/Category";
 
-import videos from "./json/db.json";
+import videos from "./json/videos.json";
+
+const categories = [
+  "Geografia",
+  "Como fazer e usar",
+  "Astronomia e Geografia",
+  "Climatologia, Meteorologia, Vegetação",
+  "Geologia e Hidrografia",
+];
+
+function filterCategory(id){
+  return videos.filter(video => video.category === categories[id]);
+}
 
 function App() {
   return (
@@ -12,10 +25,13 @@ function App() {
       <Header />
       <Banner image="home" />
       <Container>
-        <h2>Geografia</h2>
-        <section className="cards">
-          { videos.map(video => <Card id={video.id} key={video.id} />) }          
-        </section>
+          { 
+            categories.map((category, index) => (
+              <Category category={category}>
+                { filterCategory(index).map(video => <Card id={video.id} key={video.id} /> ) }
+              </Category>
+            ))
+          }
       </Container>
       <Footer />
     </>
